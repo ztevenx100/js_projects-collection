1d92ea5ad0363a298c9ca0491bc2cbdd8829174d
Pedro Steven Castiblanco <pcastiblanco@megasoft.com.co>
2024-08-25 23:39:01 -0500

    TASK(main-web): Add project files

---
+++ 00-web/src/consts.js
@@ -0,0 +1,12 @@
+export const PROJECTS = [
+    {
+      slug: "tier-list",
+      title: "Tier Maker",
+      description: "Arrastra y suelta las imágenes para crear tus propias listas de niveles",
+      learnings: [
+        "Drag & Drop",
+        "Input de imágenes",
+        "CSS Custom Properties"
+      ],
+    },
+  ]
