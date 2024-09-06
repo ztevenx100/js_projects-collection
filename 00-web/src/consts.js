f8dede1909f7f89c2feedd1bca82a51c8ee7f90d
Steven Castiblanco <50757337+ztevenx100@users.noreply.github.com>
2024-09-05 23:28:59 -0500

    FEAT(tetris): Add project

--- 00-web/src/consts.js
+++ 00-web/src/consts.js
@@ -1,5 +1,16 @@
 export const PROJECTS = [
     {
+        slug: "tetris",
+        title: "Tetris en Canvas",
+        description: "Resolvemos una prueba técnica que te propone crear el Tetris en 40 minutos",
+        learnings: [
+          "Canvas",
+          "Eventos de teclado",
+          "Lógica de programación"
+        ],
+        image: "tetris",
+    },
+    {
         slug: "tier-list",
         title: "Tier Maker",
         description: "Arrastra y suelta las imágenes para crear tus propias listas de niveles",
