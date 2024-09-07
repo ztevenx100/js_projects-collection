558f32d59ac166dfee0aa292950b67fc919397b7
Steven Castiblanco <50757337+ztevenx100@users.noreply.github.com>
2024-09-07 16:55:53 -0500

    TASK(excel): Add table and add inputs computed

--- 00-web/public/projects/excel/main.js
+++ 00-web/public/projects/excel/main.js
@@ -9,6 +9,62 @@ const ROWS = 10;
 const COLUMNS = 5;
 const FIRST_CHAR_CODE = 65;
 
-Array.from({ length:5 }, (_,i) => i).forEach(() => {
+const times = length => Array.from({ length }, (_,i) => i);
+const getColumn = i => String.fromCharCode(FIRST_CHAR_CODE + i);
 
+let selectedColumn = null;
+let STATE = times(COLUMNS).map(i => times(ROWS).map(j => ({ computedValue: 0, value: 0 })));
+
+const renderSpreadSheet = () => {
+    const headerHTML = `<tr>
+        <th></th>
+        ${times(COLUMNS).map(i => `<th>${getColumn(i)}</th>`).join('')}
+    </tr>`;
+
+    $head.innerHTML = headerHTML;
+
+    const bodyHTML = times(ROWS).map(row => {
+        return `<tr>
+            <td>${row + 1}</td>
+            ${times(COLUMNS).map(column => `
+            <td data-x="${column}" data-y="${row}">
+            <span>${STATE[column][row].computedValue}</span>
+            <input type="text" value="${STATE[column][row].value}" />
+            </td>
+            `).join('')}
+        </tr>`
+    }).join('')
+
+    $body.innerHTML = bodyHTML
+}
+
+$body.addEventListener('click', event => {
+    const td = event.target.closest('td');
+    if (!td) return;
+
+    const { x, y } = td.dataset
+    const input = td.querySelector('input');
+    const span = td.querySelector('span');
+
+    const end = input.value.length;
+    input.setSelectionRange(end, end);
+    input.focus();
+
+    $$('.selected').forEach(el => el.classList.remove('selected'));
+    selectedColumn = null;
+
+    input.addEventListener('keydown', (event) => {
+        if (event.key === 'Enter') input.blur();
+    })
+
+    input.addEventListener('blur', () => {
+        console.log({ value: input.value, state: STATE[x][y].value });
+
+        if (input.value === STATE[x][y].value) return;
+
+        updateCell({ x, y, value: input.value });
+    }, { once: true });
 })
+
+
+renderSpreadSheet();
