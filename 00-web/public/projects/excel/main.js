3ef9d1c0a5814405d9e303099d23459d689b2eac
Pedro Steven Castiblanco <pcastiblanco@megasoft.com.co>
2024-09-10 23:30:12 -0500

    TASK(excel): add excel functions

--- 00-web/public/projects/excel/main.js
+++ 00-web/public/projects/excel/main.js
@@ -10,19 +10,51 @@ const COLUMNS = 15;
 const FIRST_CHAR_CODE = 65;
 
 const times = length => Array.from({ length }, (_,i) => i);
-const getColumn = i => String.fromCharCode(FIRST_CHAR_CODE + i);
-
 let selectedColumn = null;
+let selectedRow = null;
 let STATE = times(COLUMNS).map(i => times(ROWS).map(j => ({ computedValue: 0, value: 0 })));
 
+/**
+ * Convierte un índice numérico en una representación de letra de columna (similar a las columnas de Excel).
+ * Esta función toma un índice numérico `i` y lo convierte en una letra o combinación de letras que representan
+ * una columna en una hoja de cálculo, utilizando el sistema de columnas de Excel (por ejemplo, 0 -> A, 1 -> B, 26 -> AA).
+ *
+ * @param {number} i - El índice de la columna (empezando desde 0).
+ * @returns {string} - La letra o combinación de letras que representa la columna.
+ */
+const getColumn = (i) => {
+    if(!i) return 'A';
+
+    let num = i+1;
+    if(num > 26){
+        let decena = Math.trunc(num / 26);
+        if(num % 26 === 0) decena--;
+        let unidad = num - (decena*26);
+
+        return getColumn(decena-1) + getColumn(unidad-1);
+    } else {
+        return String.fromCharCode(FIRST_CHAR_CODE + i);
+    }
+}
+
+/**
+ * Actualiza una celda específica en la hoja de cálculo con un nuevo valor y recalcula todas las celdas.
+ * Esta función crea una copia profunda del estado actual de la hoja de cálculo, actualiza la celda objetivo en las coordenadas proporcionadas (`x`, `y`) con el nuevo `valor`, calcula un nuevo valor
+ * para la celda basado en constantes proporcionadas y finalmente desencadena el recálculo y lare-renderización de todas las celdas.
+ *
+ * @param {Object} params - Los parámetros para la actualización.
+ * @param {number} params.x - La coordenada x (fila) de la celda a actualizar.
+ * @param {number} params.y - La coordenada y (columna) de la celda a actualizar.
+ * @param {*} params.value - El nuevo valor que se establecerá en la celda.
+ */
 function updateCell({ x, y, value }) {
     const newState = structuredClone(STATE);
     const constants = generateCellsConstants(newState);
 
     const cell = newState[x][y]
 
-    cell.computedValue = computeValue(value, constants); // -> span
-    cell.value = value; // -> input
+    cell.computedValue = computeValue(value, constants);
+    cell.value = value;
 
     newState[x][y] = cell;
     computeAllCells(newState, generateCellsConstants(newState));
@@ -30,25 +62,40 @@ function updateCell({ x, y, value }) {
     renderSpreadSheet();
 }
 
+/**
+ * Genera las constantes para todas las celdas de la hoja de cálculo.
+ * Este método recorre todas las celdas de la hoja de cálculo y genera una cadena de textocon la forma `const <ID de celda> = <valor computado>;` para cada celda. El ID de la celda
+ * sigue el formato de referencia de Excel (por ejemplo, A1, B2, etc.).
+ *
+ * @param {Array} cells - La matriz bidimensional que representa las celdas de la hoja de cálculo.
+ * @returns {string} - Una cadena de texto que contiene las constantes generadas para cada celda.
+ */
 function generateCellsConstants(cells) {
     return cells.map((rows, x) => {
         return rows.map((cell, y) => {
-            const letter = getColumn(x) // -> A
-            const cellId = `${letter}${y + 1}` // -> A1
-            return `const ${cellId} = ${cell.computedValue};`
+            const letter = getColumn(x);
+            const cellId = `${letter}${y + 1}`;
+            return `const ${cellId} = ${cell.computedValue};`;
         }).join('\n')
-    }).join('\n')
+    }).join('\n');
 }
 
+/**
+ * Calcula los valores computados para todas las celdas de la hoja de cálculo.
+ * Esta función recorre cada celda en la matriz de celdas y calcula su valor computado utilizando la función `computeValue` y las constantes proporcionadas. Luego, asigna
+ * el valor computado a cada celda.
+ *
+ * @param {Array} cells - La matriz bidimensional que representa las celdas de la hoja de cálculo.
+ * @param {string} constants - Las constantes generadas previamente para ser usadas en el cálculo de las celdas.
+ */
 function computeAllCells(cells, constants) {
-    // console.log('computeAllCells');
     cells.forEach((rows, x) => {
         rows.forEach((cell, y) => {
             const computedValue = computeValue(cell.value, constants)
             cell.computedValue = computedValue
         })
     });
-  }
+}
 
 function computeValue(value, constants) {
     if (typeof value === 'number') return value;
@@ -66,21 +113,20 @@ function computeValue(value, constants) {
         computedValue = `!ERROR: ${e.message}`;
     }
 
-    // console.log({ value, computedValue });
     return computedValue;
 }
 
 const renderSpreadSheet = () => {
     const headerHTML = `<tr>
         <th></th>
-        ${times(COLUMNS).map(i => `<th>${getColumn(i)}</th>`).join('')}
+        ${times(COLUMNS).map(i => `<th class="header-column" scope="col" >${getColumn(i)}</th>`).join('')}
     </tr>`;
 
     $head.innerHTML = headerHTML;
 
     const bodyHTML = times(ROWS).map(row => {
         return `<tr>
-            <td>${row + 1}</td>
+            <th class="header-row" scope="row" >${row + 1}</th>
             ${times(COLUMNS).map(column => `
             <td data-x="${column}" data-y="${row}">
             <span>${STATE[column][row].computedValue}</span>
@@ -88,16 +134,48 @@ const renderSpreadSheet = () => {
             </td>
             `).join('')}
         </tr>`
-    }).join('')
+    }).join('');
 
-    $body.innerHTML = bodyHTML
+    $body.innerHTML = bodyHTML;
 }
+
+$head.addEventListener('click', event => {
+    const th = event.target.closest('th');
+    if (!th) return;
+
+    const x = [...th.parentNode.children].indexOf(th);
+    if (x <= 0) return;
+
+    selectedColumn = x - 1;
+
+    $$('.selected').forEach(el => el.classList.remove('selected'));
+    th.classList.add('selected');
+    $$(`tr td:nth-child(${x + 1})`).forEach(el => el.classList.add('selected'));
+})
+
+$body.addEventListener('click', event => {
+    const th = event.target.closest('th');
+    console.log('table-body th ', th);
+    
+    if (!th) return;
+
+    const y = [...th.parentNode.children].indexOf(th);
+    console.log('table-body y ', y);
+    if (y < 0) return;
+
+    selectedColumn = y;
+    console.log('table-body selectedColumn ', selectedColumn);
+
+    $$('.selected').forEach(el => el.classList.remove('selected'));
+    th.classList.add('selected');
+    $$(`tr td:nth-child(${y + 1})`).forEach(el => el.classList.add('selected'));
+})
 
 $body.addEventListener('click', event => {
     const td = event.target.closest('td');
     if (!td) return;
 
-    const { x, y } = td.dataset
+    const { x, y } = td.dataset;
     const input = td.querySelector('input');
     const span = td.querySelector('span');
 
@@ -121,20 +199,6 @@ $body.addEventListener('click', event => {
     }, { once: true });
 })
 
-$head.addEventListener('click', event => {
-    const th = event.target.closest('th');
-    if (!th) return;
-
-    const x = [...th.parentNode.children].indexOf(th);
-    if (x <= 0) return;
-
-    selectedColumn = x - 1;
-
-    $$('.selected').forEach(el => el.classList.remove('selected'));
-    th.classList.add('selected');
-    $$(`tr td:nth-child(${x + 1})`).forEach(el => el.classList.add('selected'));
-})
-
 document.addEventListener('keydown', event => {
     if (event.key === 'Backspace' && selectedColumn !== null) {
         times(ROWS).forEach(row => {
@@ -153,7 +217,7 @@ document.addEventListener('copy', event => {
         event.clipboardData.setData('text/plain', columnValues.join('\n'));
         event.preventDefault();
     }
-  })
+})
 
 document.addEventListener('click', event => {
     const { target } = event;
